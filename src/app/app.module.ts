91fea3509ddb9396a2d1cc7027c7377a308b6ce6
Mateus Matos Marquizeti <mateusmarquizeti@gmail.com>
2019-06-13 11:26:13 -0300

    Adds pt-BR intl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
@@ -41,7 +41,8 @@ import { ShoppingCartService } from './restaurant-details/shopping-cart/shopping
   ],
   providers: [
     RestaurantService,
-    ShoppingCartService
+    ShoppingCartService,
+    {provide: LOCALE_ID, useValue: 'pt-BR'}
   ],
   bootstrap: [AppComponent]
 })
