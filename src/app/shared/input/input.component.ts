bd56a4ab56e3bae689f803364fcdf44c70c3afe8
Mateus Matos Marquizeti <mateusmarquizeti@gmail.com>
2019-07-05 11:22:29 -0300

    Refactor input component

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
-  selector: 'mt-input',
+  selector: 'mt-input-container',
   templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
+
+  @ContentChild(NgModel) model: NgModel;
+
+  @Input ()label: string;
+  @Input() errorMessage: string;
+
+  input: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    this.input = this.model;
+
+    if (this.input === undefined) {
+      throw new Error('This component must be used with ngModel directive');
+    }
+  }
+
+  hasSuccess(): boolean {
+    return this.input.valid && (this.input.dirty || this.input.touched);
+  }
+
+  hasError(): boolean {
+    return this.input.invalid && (this.input.dirty || this.input.touched);
   }
 
 }
